8abf99a8137ef247b760898928b6f3c3d0923502
Alexis Hernandez <alexis22229@gmail.com>
2018-12-24 18:33:51 -0700

    play: Update codecs

---
+++ playsonify-play/src/com/alexitc/playsonify/play/codecs/DefaultCodecs.scala
@@ -0,0 +1,50 @@
+package com.alexitc.playsonify.play.codecs
+
+import com.alexitc.playsonify.models.pagination.{Count, Limit, Offset, PaginatedResult}
+import com.alexitc.playsonify.models.{WrappedInt, WrappedLong, WrappedString}
+import org.scalactic.Every
+import play.api.libs.json._
+
+trait DefaultCodecs {
+
+  implicit val countWrites: Writes[Count] = Writes[Count] { count => JsNumber(count.int) }
+
+  implicit val limitWrites: Writes[Limit] = Writes[Limit] { limit => JsNumber(limit.int) }
+
+  implicit val offsetWrites: Writes[Offset] = Writes[Offset] { offset => JsNumber(offset.int) }
+
+  implicit def paginatedResultWrites[T](implicit writesT: Writes[T]): Writes[PaginatedResult[T]] = OWrites[PaginatedResult[T]] { result =>
+    Json.obj(
+      "offset" -> result.offset,
+      "limit" -> result.limit,
+      "total" -> result.total,
+      "data" -> result.data
+    )
+  }
+
+  implicit val wrappedIntWrites: Writes[WrappedInt] = {
+    Writes[WrappedInt] { wrapped => JsNumber(wrapped.int) }
+  }
+
+  implicit val wrapperLongWrites: Writes[WrappedLong] = {
+    Writes[WrappedLong] { wrapped => JsNumber(wrapped.long) }
+  }
+
+  implicit val wrappedStringWrites: Writes[WrappedString] = {
+    Writes[WrappedString] { wrapped => JsString(wrapped.string) }
+  }
+
+  implicit def everyReads[T](implicit readsT: Reads[T]): Reads[Every[T]] = Reads[Every[T]] { json =>
+    json
+        .validate[List[T]]
+        .flatMap { list =>
+          Every.from(list)
+              .map(JsSuccess.apply(_))
+              .getOrElse {
+                JsError.apply("A non-empty list is expected")
+              }
+        }
+  }
+}
+
+object DefaultCodecs extends DefaultCodecs
